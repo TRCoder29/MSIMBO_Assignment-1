b00b2853659b24b659b38c72826b9787a371683b
TSN Student <student@ulem.org>
2018-08-15 11:46:09 -0400

    Attempt 3: mlab update and widget-edit hold-over

--- server/model/models.server.js
+++ server/model/models.server.js
@@ -5,9 +5,8 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds111562.mlab.com:11562/heroku_xhtkg1lx'; // use yours
+  connectionString += '@ds119732.mlab.com:19732/heroku_hn7k15dr'; // use yours
 }
-
 
 var mongoose = require("mongoose");
 var db = mongoose.connect(connectionString);
